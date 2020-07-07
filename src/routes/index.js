63bb2ad4eed1f5dd75b3765da9ea9da9990896af
Madalina Rosca <mada.rosca@yahoo.com>
2020-07-07 17:01:36 +0300

    use routes constants

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history'
 import Main from '../containers/Main'
 import About from '../containers/views/AboutPage'
 import DnD from '../containers/views/DnD'
+import { DND, ABOUT } from '../constants/routes'
 
 const browserHistory = createBrowserHistory()
 
@@ -13,8 +14,9 @@ export default class Routes extends Component {
 			<HashRouter basename='/'>
 				<Main>
 					<Switch>
-						<Route path={'/'} exact component={About} />
-						<Route path={'/dnd'} component={DnD} />
+						<Route path={ABOUT} exact component={About} />
+						<Route path={DND} component={DnD} />
+						<Redirect from='/' to={ABOUT} />
 					</Switch>
 				</Main>
 			</HashRouter>
