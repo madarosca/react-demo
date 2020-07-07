ec75e5d2043212d94ba887c1f9f2bd8f166a8040
Madalina Rosca <mada.rosca@yahoo.com>
2020-07-07 17:25:17 +0300

    change nav link name

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -37,7 +37,7 @@ const NavBar = (props) => {
 							Resume
 						</NavLink>
 						<NavLink className='nav-item nav-link' to={CONTACT} onClick={() => { }}>
-							Contact me
+							Contact
 						</NavLink>
 					</ul>
 				</div>
