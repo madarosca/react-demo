e83860fd84fe770e74b1e516b9befb814cde0781
Madalina Rosca <mada.rosca@yahoo.com>
2020-07-07 16:10:29 +0300

    Updates

--- precache-manifest.8d668c4521e1a7543782ea95361eae25.js
+++ precache-manifest.8d668c4521e1a7543782ea95361eae25.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32dd30e462804c8cb785431f3fcd9aa0",
+    "revision": "1d2bcb856ab586deb41f314923089545",
     "url": "/react-demo/index.html"
   },
   {
-    "revision": "58d1c6967b0dbe45b759",
+    "revision": "7964686e7f2b0000a806",
     "url": "/react-demo/static/css/main.e7f2fd5e.chunk.css"
   },
   {
-    "revision": "d92bcfa81bdacb5442ed",
-    "url": "/react-demo/static/js/2.88d5c427.chunk.js"
+    "revision": "c98aeec24d7fda207c85",
+    "url": "/react-demo/static/js/2.be48e2f1.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-demo/static/js/2.88d5c427.chunk.js.LICENSE.txt"
+    "url": "/react-demo/static/js/2.be48e2f1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58d1c6967b0dbe45b759",
-    "url": "/react-demo/static/js/main.813d1713.chunk.js"
+    "revision": "7964686e7f2b0000a806",
+    "url": "/react-demo/static/js/main.793fe96e.chunk.js"
   },
   {
     "revision": "e91f5e23d011ab615da4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-demo/precache-manifest.ab05ce09c9ce02d490b677b183615676.js"
+  "/react-demo/precache-manifest.8d668c4521e1a7543782ea95361eae25.js"
 );
 
 self.addEventListener('message', (event) => {
