2bf6635b056bf804604c1c61551a7a730d93d222
Madalina Rosca <mada.rosca@yahoo.com>
2020-07-07 14:50:45 +0300

    Updates

---
+++ precache-manifest.f14610f0ebc41044151640d98f73c67b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80ce57416635bf57e740176d91076a9",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "f477c339d35f98e3d791",
+    "url": "/react-demo/static/css/main.e7f2fd5e.chunk.css"
+  },
+  {
+    "revision": "f9e9f67bad663953fe5d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f477c339d35f98e3d791",
+    "url": "/react-demo/static/js/main.c00b2536.chunk.js"
+  },
+  {
+    "revision": "e91f5e23d011ab615da4",
+    "url": "/react-demo/static/js/runtime-main.5f8d9a6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
